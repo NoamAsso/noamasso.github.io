9f607d449b7bb0300a7912d804dfe9199ac23f74
NoamAsso <34792669+NoamAsso@users.noreply.github.com>
2023-07-31 22:23:50 +0300

    Update backend.js

--- backend.js
+++ backend.js
@@ -14,47 +14,20 @@ firebase.initializeApp(firebaseConfig);
 // Get a reference to the Realtime Database
 const database = firebase.database();
 
-// Get references to the form and checkbox elements
-const myForm = document.getElementById("myForm");
+// Get a reference to the checkbox element
 const checkbox = document.getElementById("myCheckbox");
-
-// Function to display the message
-function showMessage() {
-    const messageParagraph = document.getElementById("message");
-    messageParagraph.textContent = "Hi, my name is Noam";
-}
 
 // Read the data from the Realtime Database and set the checkbox state on page load
 database.ref("checkboxState").on("value", function(snapshot) {
     const isChecked = snapshot.val();
     checkbox.checked = isChecked;
-
-    // If the checkbox is checked, display the message
-    if (isChecked) {
-        showMessage();
-    } else {
-        // Clear the message if the checkbox is unchecked
-        const messageParagraph = document.getElementById("message");
-        messageParagraph.textContent = "";
-    }
 });
 
-// Add an event listener to the form's submit event
-myForm.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting and page reloading
-
+// Add an event listener to the checkbox
+checkbox.addEventListener("change", function() {
     // Get the checkbox state (true for checked, false for unchecked)
     const isChecked = checkbox.checked;
 
     // Update the data in the Realtime Database
     database.ref("checkboxState").set(isChecked);
-
-    // If the checkbox is checked, display the message
-    if (isChecked) {
-        showMessage();
-    } else {
-        // Clear the message if the checkbox is unchecked
-        const messageParagraph = document.getElementById("message");
-        messageParagraph.textContent = "";
-    }
 });
